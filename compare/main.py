0d48b83cf055d7c68357ae44941ba1c30eee1188
ramoonmedeiro <r.medeiro10@gmail.com>
2022-10-28 16:07:39 -0300

    Monstando scraping para previsoes

---
+++ compare/main.py
@@ -0,0 +1,5 @@
+import tool
+
+tool.scrape()
+
+#tool.results()
---
+++ compare/tool.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from time import sleep
+import os
+
+def scrape():
+	servico = Service(GeckoDriverManager().install())
+	navegador = webdriver.Firefox(service=servico)
+
+	url = f'https://www.nba.com/stats/players/traditional?sort=PTS&dir=-1&Season=2022-23&SeasonType=Regular+Season'
+	navegador.get(url)
+	sleep(7)
+	element = navegador.find_element(By.XPATH, "//div[@class='Crom_base__f0niE']")
+	#element = navegador.find_element(By.XPATH, "//div[@class='nba-stat-table']") new name to <div><table>
+	html = element.get_attribute('outerHTML')
+	bs = BeautifulSoup(html, 'html.parser')
+	table = bs.find(name='table')
+
+
+	df = pd.read_html(str(table))[0]
+	del df['Unnamed: 0']
+	df_full = df[['Player', 'Min', 'PTS', 'AST', 'REB', 'FG%', 'FT%']].head(50).copy()
+	df_full.to_csv(f'prever.csv', index=False)
+
+	navegador.quit()
+
+	return
+
+def results():
+	pass
+	
