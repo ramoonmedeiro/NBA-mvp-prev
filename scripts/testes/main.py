45fa579808c5461a571eb13fe485832eed5fdb1d
ramoonmedeiro <r.medeiro10@gmail.com>
2022-11-07 19:01:05 -0300

    Mudando algumas funções e mudando para POO

--- scripts/testes/main.py
+++ scripts/testes/main.py
@@ -1,9 +1,13 @@
-import results
+# Lib criada
+from results import Baseline, Melhores, Otimizacao
+
+# Manipulação de dados
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+
+# Separação entre treino e teste
+from sklearn.model_selection import train_test_split
 
 # Modelos e scalers
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -23,11 +27,38 @@ y = df['MVP']
 X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size=0.2, random_state=99)
 
 # Definição dos modelos e scalers
-modelos = [LogisticRegression(), SVC(probability=True), KNeighborsClassifier(), GaussianNB(), DecisionTreeClassifier(), RandomForestClassifier()]
+modelos = [LogisticRegression(), SVC(probability=True), KNeighborsClassifier(), 
+            GaussianNB(), DecisionTreeClassifier(), RandomForestClassifier()]
+
 # Mdoelos com melhores desempenhos com hiperparametros default.
-modelos_balanced = [SVC(probability=True, class_weight='balanced'), DecisionTreeClassifier(class_weight='balanced'), RandomForestClassifier(class_weight='balanced')]
+modelos_balanced = [SVC(probability=True, class_weight='balanced'), DecisionTreeClassifier(class_weight='balanced'), 
+                    RandomForestClassifier(class_weight='balanced')]
+
 scalers = [StandardScaler(), MinMaxScaler()]
 
-# Chamando funções e armazenando informações com o MLFlow
-results.testes(X_treino, y_treino, k = 7, modelos = modelos, scalers = scalers, scoring = 'recall')
-results.testes(X_treino, y_treino, k = 7, modelos = modelos_balanced, scalers = scalers, scoring = 'recall')
+# Instanciação e armazenando informações com o MLFlow
+baseline = Baseline(X_treino, y_treino, k = 7, modelos = modelos, scalers = scalers, scoring = 'recall', name='Baseline')
+best = Melhores(X_treino, y_treino, k = 7, modelos = modelos_balanced, scalers = scalers, scoring = 'recall', name='Melhores') 
+#baseline.testes()
+#best.testes()
+
+
+# Carregando o melhor modelo com hiperparametros default
+
+modelo = joblib.load('model.pkl')
+
+# Etapa de otimização com GridSearchCV
+
+params = { 
+    'model__C': [0.01, 1, 10, 100, 1000],  
+   'model__gamma': [1, 0.1, 0.01, 0.001, 0.0001], 
+   'model__kernel': ['rbf','linear','sigmoid']
+}
+
+
+# Instaciacao
+grid = Otimizacao(X_treino, y_treino, modelo, scoring)
+
+#Apresentado melhores parametros e resultado.
+grid.opt()
+
--- scripts/testes/results.py
+++ scripts/testes/results.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 
+# Joblib lib
+import joblib
 
 # Libs do sklearn
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
@@ -21,12 +23,24 @@ import mlflow.sklearn
 
 
 # Iniciando modelo baseline
-def testes(X_treino, y_treino, k, modelos, scalers, scoring):
 
-        for i in range(k, 0, -1):
-            for scaler in scalers:
-                for modelo in modelos:
-                    mlflow.set_experiment('MELHORES')
+class Baseline():
+
+    def __init__(self, X_treino, y_treino, k, modelos, scalers, scoring, name):
+        self.X_treino = X_treino
+        self.y_treino = y_treino
+        self.k = k
+        self.modelos = modelos
+        self.scalers = scalers
+        self.scoring = scoring
+        self.name = name
+
+
+    def testes(self):
+        for i in range(self.k, 0, -1):
+            for scaler in self.scalers:
+                for modelo in self.modelos:
+                    mlflow.set_experiment(f'{self.name}')
                     with mlflow.start_run():
 
                         pipe = Pipeline(steps=[
@@ -35,7 +49,32 @@ def testes(X_treino, y_treino, k, modelos, scalers, scoring):
                             ('model', modelo)
                         ])
                         skfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=99)
-                        resultados = cross_val_score(pipe, X_treino, y_treino, cv=skfold, scoring=scoring)
+                        resultados = cross_val_score(pipe, self.X_treino, self.y_treino, cv=skfold, scoring=self.scoring)
                         mlflow.log_metric('Recall', round(resultados.mean()*100,2))
                         mlflow.sklearn.log_model(pipe, 'modelo')
                     mlflow.end_run()
+
+class Melhores(Baseline):
+
+    def __init__(self, X_treino, y_treino, k, modelos, scalers, scoring, name):
+        super().__init__(X_treino, y_treino, k, modelos, scalers, scoring, name)
+
+
+class Otimizacao(Baseline):
+
+    def __init__(self, X_treino, y_treino, modelo, scoring):
+        super().__init__(X_treino, y_treino, modelo, scoring, params)
+        self.params = params
+
+
+    def opt(self):
+        pipe_best = self.modelo
+        pipe_grid = GridSearchCV(self.modelo, self.params , scoring=self.scoring, cv=5)
+        pipe_grid.fit(X_treino, y_treino)
+
+        # Apresentado resultados
+        print(pipe_grid.best_params_)
+        print(pipe_grid.best_score_)
+        
+
+
