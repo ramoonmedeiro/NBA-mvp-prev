b5fe2d54f22b6defab11a8f3e583e93023a26529
ramoonmedeiro <r.medeiro10@gmail.com>
2022-11-07 22:12:49 -0300

    Adicionando nova feature ao modelo em pordução

--- deploy/app.py
+++ deploy/app.py
@@ -3,10 +3,10 @@ import numpy as np
 import joblib
 from sklearn.preprocessing import StandardScaler
 
-def previsao(MIN, PTS, AST, REB, FG, FT):
-	model = joblib.load('modelo-versao2.pkl')
+def previsao(MIN, RATE_WIN , PTS, AST, REB, FG, FT):
+	model = joblib.load('model-best.pkl.pkl')
 
-	val = np.array([MIN, PTS, AST, REB, FG, FT]).reshape(1,-1)
+	val = np.array([MIN, RATE_WIN, PTS, AST, REB, FG, FT]).reshape(1,-1)
 	prev = model.predict_proba(val)
 
 	return {"Chances de ser MVP": prev[0][1]}
@@ -14,6 +14,7 @@ def previsao(MIN, PTS, AST, REB, FG, FT):
 demo = gr.Interface(fn=previsao,
 					inputs=["number",
 					"number",
+					"number"
 					"number",
 					"number",
 					"number",
